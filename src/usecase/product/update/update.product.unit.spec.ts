8e6a103d31f5b72147739eb5b7ed4f1472fe9bb2
alannunes <alan.nunes@luizalabs.com>
2023-01-26 22:34:25 -0300

    feat: create usecases to product

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,34 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import { InputUpdateProductDto } from "./update.product.dto";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = ProductFactory.createProductA(
+  "Refrigerante",
+  10.99
+);
+
+const input: InputUpdateProductDto = {
+  id: product.id,
+  name: product.name,
+  price: product.price
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test for product update use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
